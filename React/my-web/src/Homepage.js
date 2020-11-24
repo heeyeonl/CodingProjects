f40046d37f6dbb04dda38b2196f4c745cf9393c5
Heeyeon Lee <heeyoenl@byu.edu>
2020-11-24 02:24:35 -0700

    Hello world

---
+++ React/my-web/src/Homepage.js
@@ -0,0 +1,16 @@
+import './Homepage.css';
+
+function Homepage() {
+    return (
+      <div className="Homepage">
+        <header className="Homepage-header">
+          <p>
+            Hello World
+          </p>
+        </header>
+      </div>
+    )
+  }
+  
+  export default Homepage;
+  
